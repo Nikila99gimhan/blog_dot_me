2f741c44dbf73d4f5ad420beed327171b76ff3b4
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2026-06-27 19:00:17 +0530

    Enable mobile hamburger menu by removing DesktopOnly from Explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
@@ -49,7 +49,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [],
 }
